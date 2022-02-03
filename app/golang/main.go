e3152bc87f6c8a2ee7a49e10ff17bec07f79f16f
Dmitry Zavedeev <dmitry.zavedeev@aventus.work>
2022-02-03 17:18:52 +0300

    try log with json data

--- app/golang/main.go
+++ app/golang/main.go
@@ -4,7 +4,8 @@ import (
         "fmt"
 		"io"
         "os"
-		"time" // https://pkg.go.dev/time
+		// "time" // https://pkg.go.dev/time
+		"encoding/json"
 )
 
 func main() {
@@ -14,10 +15,45 @@ func main() {
 		io.WriteString(os.Stdout,"This is the line to standard output.\n")
 		io.WriteString(os.Stderr,"This is the line for standard error output.\n")
 
+		// --- time output constant --- //
 		// print every 5 seconds how long the program is running
-		for range time.Tick(time.Second * 30) {
-			go func() {
-				fmt.Println(os.Stdout, time.Now())
-			}()
+		// for range time.Tick(time.Second * 30) {
+		// 	go func() {
+		// 		fmt.Println(os.Stdout, time.Now())
+		// 	}()
+		// }
+
+		// --- JSON --- //
+		data_log := map[string]interface{}{
+			"@timestamp": 2022-02-01T08:50:02.711889+00:00,
+			"@source": "b58a81938fbf",
+			"@fields": "",
+			"@message": "Api request",
+			"@tags": ["api-request-logger"],
+			"@type": "api-request-response",
 		}
+
+
+		// https://go.dev/play/p/LY0RWyrT6r4
+		res := map[string]interface{}{
+			"one": 1,
+			"two": "twotwo",
+		}
+		if err := json.NewEncoder(os.Stdout).Encode(res); err != nil {
+			panic(err)
+		}
+		// output:
+		// {"one":1,"two":"twotwo"}
+
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent("", "  ")
+		if err := enc.Encode(res); err != nil {
+			panic(err)
+		}
+		// output:
+		// {
+		// 	"one": 1,
+		// 	"two": "twotwo"
+		// }
 }
+
